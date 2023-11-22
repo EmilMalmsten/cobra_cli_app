d7236cb8cf753e4e1f4a9bb65d4ed444e5b54254
Emil Malmsten <emil_malmsten@hotmail.com>
2023-11-22 12:26:12 +0700

    just some changes

--- main.go
+++ main.go
@@ -4,18 +4,9 @@ Copyright © 2023 Emil M
 package main
 
 import (
-	"fmt"
-
 	"github.com/limesten/cobra_cli_app/cmd"
 )
 
-var (
-	version = "dev"
-	commit  = "none"
-	date    = "unknown"
-)
-
 func main() {
-	fmt.Printf("version is: %s", version)
 	cmd.Execute()
 }
