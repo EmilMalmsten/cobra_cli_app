8f3c6eb0bc4e409e12265322221645a2ad02c2dd
Emil Malmsten <emil_malmsten@hotmail.com>
2023-11-22 14:06:35 +0700

    fix version

--- cmd/version.go
+++ cmd/version.go
@@ -2,12 +2,23 @@ package cmd
 
 import (
 	"fmt"
+	"runtime/debug"
 
 	"github.com/spf13/cobra"
 )
 
 // GoBlueprintVersion is the version of the cli to be overwritten by goreleaser in the CI run with the version of the release in github
 var GoBlueprintVersion string
+
+func getGoBlueprintVersion() string {
+	if len(GoBlueprintVersion) != 0 {
+		return GoBlueprintVersion
+	}
+	if info, ok := debug.ReadBuildInfo(); ok {
+		return info.Main.Version
+	}
+	return "unknown"
+}
 
 // versionCmd represents the version command
 var versionCmd = &cobra.Command{
@@ -16,6 +27,7 @@ var versionCmd = &cobra.Command{
 	Long: `The version command provides information about the application's version.
 Use this command to check the current version of the application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Printf("cobra cli version %v\n", GoBlueprintVersion)
+		goBlueprintVersion := getGoBlueprintVersion()
+		fmt.Printf("cobra cli version %v\n", goBlueprintVersion)
 	},
 }
